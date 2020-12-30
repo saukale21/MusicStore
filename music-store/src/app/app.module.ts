3fd9da046720c53239b88e80553144a83ccdad96
aditya-naikk <46914324+aditya-naikk@users.noreply.github.com>
2020-12-30 21:59:29 +0530

    Added cart component

--- music-store/src/app/app.module.ts
+++ music-store/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginModule } from '../login/login.module';
 import { HomeModule } from '../home/home.module';
 import { HomeComponent } from '../home/components/home/home.component';
+import { CartComponent} from '../cart/cart.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LoginModule,
     HomeModule,
+    MDBBootstrapModule.forRoot()
 
   ],
   providers: [],
