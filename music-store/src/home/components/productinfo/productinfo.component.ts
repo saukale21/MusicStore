cabae80f85ac9932d597473a77d2970f94514495
Saurabh Kale <saurabhkale21@gmail.com>
2021-01-06 17:44:25 +0530

    ProductInfo Changes

--- music-store/src/home/components/productinfo/productinfo.component.ts
+++ music-store/src/home/components/productinfo/productinfo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from 'src/home/services/products.service';
+import { environment } from '../../../environments/environment.prod';
 
 
 @Component({
@@ -9,36 +10,39 @@ import { ProductsService } from 'src/home/services/products.service';
   styleUrls: ['./productinfo.component.css']
 })
 export class ProductInfoComponent implements OnInit {
-  Imagepath: string;
-  productInfo:Array<any> = [];
+  //Imagepath: string;
+  //productInfo:Array<any> = [];
   id: String;
   data:any;
-  res : any;
+  res = {};
+  imageurl = environment.URL;
   constructor(private route: ActivatedRoute, private productservice: ProductsService) { }
 
   ngOnInit(): void
   {
-    this.Imagepath = '../../assets/img/guitar3.jpg';
+    //this.Imagepath = '../../assets/img/guitar3.jpg';
     this.id = this.route.snapshot.params['id'];
-    this.getProductById();
-    // console.log(this.id);
-    // this.productservice.getProductById(this.id).subscribe(res =>
-    // {
-    //   this.res=res;
-    //   console.log(this.res);
-
-    // });
-  }
-
-  getProductById()
-  {
-      this.productservice.getProductById(this.id).subscribe(res =>{
-        this.res=res;
-        this.productInfo = res;
-        console.log(this.productInfo[0].product);
+    // this.getProductById();
+    console.log(this.id);
+    this.productservice.getProductById(this.id).subscribe(res =>
+    {
+        this.res=res.product;
+        //this.productInfo = res;
+        //console.log(this.productInfo[0].product);
         console.log(this.res);
-      });
+
+    });
   }
+
+  // getProductById()
+  // {
+  //     this.productservice.getProductById(this.id).subscribe(res =>{
+  //       this.res=res;
+  //       this.productInfo = res;
+  //       console.log(this.productInfo[0].product);
+  //       console.log(this.res);
+  //     });
+  // }
 
   
 
